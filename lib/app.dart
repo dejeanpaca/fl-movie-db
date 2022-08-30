931c2fbe7c3479dacfbc578ee1af28071b6afade
Dejan Boras <de.jean.7777@protonmail.com>
2022-08-31 00:09:58 +0200

    Keep language code as app property

--- lib/app.dart
+++ lib/app.dart
@@ -2,5 +2,6 @@ import 'package:moviedb/services/moviedb.dart';
 
 class App {
   static const String title = 'MovieDB';
+  static const String language = 'en_US';
   static MovieDb movieDb = MovieDb();
 }
