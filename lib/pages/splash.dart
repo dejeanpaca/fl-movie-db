a4b7e0e8885e43eb6cc3bc9b8986777511ccb2b4
Dejan Boras <de.jean.7777@protonmail.com>
2022-08-30 21:07:28 +0200

    Move init logic into separate file and initialize the db

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:moviedb/pages/home.dart';
+import 'package:moviedb/services/load.dart';
 import 'package:moviedb/ui/theme/theme_list.dart';
 import 'package:moviedb/utils/single_fire.dart';
 import 'package:provider/provider.dart';
@@ -17,7 +16,7 @@ class SplashPageState extends State<SplashPage> {
 
   @override
   Widget build(BuildContext context) {
-    loadFire.fire(load);
+    loadFire.fire(() => load(context));
 
     var theme = Provider.of<ThemeList>(context).current;
     print(theme.name);
@@ -29,16 +28,5 @@ class SplashPageState extends State<SplashPage> {
         Center(child: Image(width: 120.0, height: 120.0, image: AssetImage('assets/images/logo.png'))),
       ])),
     );
-  }
-
-  /// load our startup state
-  Future<void> load() async {
-    // portrait-only mode
-    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-
-    var navigator = Navigator.of(context);
-    await Future.delayed(const Duration(milliseconds: 250));
-
-    if (navigator.mounted) navigator.pushReplacement(MaterialPageRoute(builder: (context) => const HomePage()));
   }
 }
---
+++ lib/services/load.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:moviedb/app.dart';
+import 'package:moviedb/pages/home.dart';
+
+import 'db.dart';
+
+/// load our startup state
+Future<void> load(BuildContext context) async {
+  var navigator = Navigator.of(context);
+
+  // portrait-only mode
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+  await Db.createStorage();
+  await App.movieDb.initialize();
+
+  await Future.delayed(const Duration(milliseconds: 250));
+
+  if (navigator.mounted) navigator.pushReplacement(MaterialPageRoute(builder: (context) => const HomePage()));
+}
