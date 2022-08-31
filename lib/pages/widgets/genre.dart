974a8e3095a7626bafdbbd725b8d8e6148c5dce0
Dejan Boras <de.jean.7777@protonmail.com>
2022-08-31 17:20:22 +0200

    Genre widget can be given custom fontSize and padding

--- lib/pages/widgets/genre.dart
+++ lib/pages/widgets/genre.dart
@@ -5,20 +5,23 @@ import 'package:provider/provider.dart';
 
 class GenreTag extends StatelessWidget {
   final Genre genre;
+  final double? fontSize;
+  final EdgeInsets? padding;
 
-  const GenreTag({Key? key, required this.genre}) : super(key: key);
+  const GenreTag({Key? key, required this.genre, this.fontSize, this.padding}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     var theme = Provider.of<ThemeList>(context).current;
 
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 12.5, vertical: 7.5),
+      padding: padding ?? const EdgeInsets.symmetric(horizontal: 12.5, vertical: 7.5),
       decoration: BoxDecoration(
-        borderRadius: const BorderRadius.all(Radius.circular(10)),
+        borderRadius: const BorderRadius.all(Radius.circular(5)),
         color: theme.genreTagColor,
       ),
-      child: Text(genre.name, style: theme.genreStyle),
+      child:
+          Text(genre.name, style: fontSize != null ? theme.genreStyle.copyWith(fontSize: fontSize!) : theme.genreStyle),
     );
   }
 }
