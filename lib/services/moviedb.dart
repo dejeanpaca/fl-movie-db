61fc0abc1b36244ff7d3b8de7c9028e1d966a3e8
Dejan Boras <de.jean.7777@protonmail.com>
2022-08-31 23:23:27 +0200

    Simple but destructive way to upgrade database

--- lib/services/moviedb.dart
+++ lib/services/moviedb.dart
@@ -6,16 +6,12 @@ import 'db.dart';
 
 class MovieDb extends Db {
   MovieDb() : super('movie.db') {
-    version = 2;
+    version = 3;
   }
 
   @override
   Future<Database> open() async {
-    return await openDatabase(
-      await constructPath(),
-      version: version,
-      onCreate: onCreate,
-    );
+    return await openDatabase(await constructPath(), version: version, onCreate: onCreate, onUpgrade: onUpgrade);
   }
 
   FutureOr<void> onCreate(Database db, int version) async {
@@ -28,6 +24,20 @@ class MovieDb extends Db {
 
     // table for genres
     await db.execute('CREATE TABLE genres (id INTEGER PRIMARY KEY, genre_id INTEGER, name TEXT)');
+
+    print('Created $dbName version $version');
+  }
+
+  FutureOr<void> onUpgrade(Database db, int oldVersion, int newVersion) async {
+    // simplest migration is starting anew
+    db.execute('DROP TABLE IF EXISTS favourites');
+    db.execute('DROP TABLE IF EXISTS movie_genres');
+    db.execute('DROP TABLE IF EXISTS genres');
+
+    // recreate
+    await onCreate(db, version);
+
+    print('Upgraded database to version $newVersion from $oldVersion');
   }
 
   /// execute an sql query
